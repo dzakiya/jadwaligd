269a3d752f71b96c84c7335c8d0d9938bbbc6359
Dzakiyatur Rosyidah <zakia.rosi@gmail.com>
2024-05-15 10:20:22 +0700

    update v-list

--- components/card3.vue
+++ components/card3.vue
@@ -1,6 +1,5 @@
 <template>
 
-     
         <div class="d-flex flex-no-wrap justify-space-between">
           <v-row align="center" justify="center" dense>
             <v-col v-for="(ksm, id) in jmlKsm.data" :key="id" cols="12" md="3"> 
@@ -10,51 +9,47 @@
                 height="500"
                 width="320"
                 color="surface-variant"
-                :title="ksm.Nama_ksm"
+                :title="ksm.Nama_ksm" 
                 :subtitle="ksm.id"
                 append-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
                 prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
               >
-                <v-card-text v-for="(jj, id) in jadwal.data" :key="id">
+                <!-- <v-card-text v-for="(jj, id) in jadwal.data" :key="id">
                     {{ jj.Nama_petugas }}
-                </v-card-text>
-                <template v-slot:actions>
-                  <v-btn text="Button"></v-btn>
-                </template>
+                </v-card-text> -->
+            
+              <v-list>
+                <v-list-item
+          v-for="(item, id) in jadwal.data"
+          :key="item.id"
+          :subtitle="item.Nama_petugas"
+          :title="item.Level.Nama_level_igd"
+        >
+          <template v-slot:prepend>
+            <v-avatar color="blue">
+              <v-icon color="white" icon="mdi-hospital-box"></v-icon>
+            </v-avatar>
+          </template>
+  
+          <template v-slot:append>
+            <v-btn
+              color="grey-lighten-1"
+              icon="mdi-information"
+              variant="text"
+            ></v-btn>
+          </template>
+        </v-list-item>
+              </v-list> 
+          
+
               </v-card>
   
               <div class="text-center text-caption">{{ ksm }}</div>
             </v-col>
           </v-row>
         </div>
-     
 
-  
-    <v-col v-for="(jdw, id) in jadwal.data[0]" :key="id" cols="12" md="3">
-      <v-card
-        subtitle="Isi dari card nanti ditaruh sini ya, nama dokter, level, dsb"
-        text="TEXT AREA DISINI"
-        append-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
-        prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
-      >
-      <v-card-title>
-          {{jdw}}
-        </v-card-title>
-      </v-card>
-  
-      <div class="text-center text-caption">{{ jdw }}</div>
-    </v-col>
-  
-    <pre>{{ jadwal.data[0] }} </pre>
-    {{ jadwal.data[0].Nama_petugas }}
-    <br />
-    {{ jadwal.data.length }}
-    <br />
-    {{ now }}
-  
-    {{ tanggal }}
-    {{ bulan }}
-    {{ tahun }}
+        <pre>{{ jadwal }}</pre>
   </template>
   
   <script setup>
@@ -83,11 +78,15 @@
   //coba panggil dari dari API
   //const { data: ksm, pending, error } = await useFetch(() => `https://satu.dev.rssa.id/items/data_jadwal_jaga_dokter?fields=Nama_petugas,Ksm.Nama_ksm,Level.Nama_level_igd,Jaga_awal,Jaga_akhir&filter[day(Jaga_awal)][_eq]=14&filter[month(Jaga_awal)][_eq]=05&filter[year(Jaga_awal)][_eq]=2024`)
   //const { data: ksm, pending, error } = await useFetch(() => `https://satu.dev.rssa.id/items/data_jadwal_jaga_dokter?fields=Nama_petugas,Ksm.Nama_ksm,Level.Nama_level_igd,Jaga_awal,Jaga_akhir&filter[day(Jaga_awal)][_eq]=14&filter[month(Jaga_awal)][_eq]=05&filter[year(Jaga_awal)][_eq]=2024`)
+
+  const { data: dataJadwal } = await useFetch(`https://satu.dev.rssa.id/items/data_jadwal_jaga_dokter?fields=id,Nama_petugas,Ksm.Nama_ksm,Level.Nama_level_igd,Jaga_awal,Jaga_akhir&filter[day(Jaga_awal)][_eq]=${tgl}&filter[month(Jaga_awal)][_eq]=${bln}&filter[year(Jaga_awal)][_eq]=${thn}`);
   
   const jadwal = await $fetch(
     `https://satu.dev.rssa.id/items/data_jadwal_jaga_dokter?fields=id,Nama_petugas,Ksm.Nama_ksm,Level.Nama_level_igd,Jaga_awal,Jaga_akhir&filter[day(Jaga_awal)][_eq]=14&filter[month(Jaga_awal)][_eq]=05&filter[year(Jaga_awal)][_eq]=2024`
   );
 
   const jmlKsm = await $fetch(`https://satu.dev.rssa.id/items/daftar_ksm`);
+
+
   </script>
   
--- pages/jadwal.vue
+++ pages/jadwal.vue
@@ -1,16 +1,99 @@
-<script setup lang="ts">
-    import { useNow, useDateFormat } from '@vueuse/core'
-    const now = useDateFormat(useNow(), "dddd, DD MMMM YYYY HH:mm:ss", {locales:'id-ID'})
-
-    const data = await useFetch('https://satu.dev.rssa.id/items/data_jadwal_jaga_dokter')
-
-    const data_ksm = await useFetch('https://satu.dev.rssa.id/items/daftar_ksm')
-    
-
-</script>
-
 <template>
-{{ data }}
+    <v-card
+      class="mx-auto"
+      max-width="600"
+    >
+      <v-toolbar color="secondary">
+        <v-btn icon="mdi-menu" variant="text"></v-btn>
+  
+        <v-toolbar-title>My files</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn icon="mdi-magnify" variant="text"></v-btn>
+  
+        <v-btn icon="mdi-view-module" variant="text"></v-btn>
+      </v-toolbar>
+  
+      <v-list lines="two">
+        <v-list-subheader inset>Folders</v-list-subheader>
+  
+        <v-list-item
+          v-for="folder in folders"
+          :key="folder.title"
+          :subtitle="folder.subtitle"
+          :title="folder.title"
+        >
+          <template v-slot:prepend>
+            <v-avatar color="grey-lighten-1">
+              <v-icon color="white">mdi-folder</v-icon>
+            </v-avatar>
+          </template>
+  
+          <template v-slot:append>
+            <v-btn
+              color="grey-lighten-1"
+              icon="mdi-information"
+              variant="text"
+            ></v-btn>
+          </template>
+        </v-list-item>
+  
+        <v-divider inset></v-divider>
+  
+        <v-list-subheader inset>Files</v-list-subheader>
+  
+        <v-list-item
+          v-for="file in files"
+          :key="file.title"
+          :subtitle="file.subtitle"
+          :title="file.title"
+        >
+          <template v-slot:prepend>
+            <v-avatar :color="file.color">
+              <v-icon color="white">{{ file.icon }}</v-icon>
+            </v-avatar>
+          </template>
+  
+          <template v-slot:append>
+            <v-btn
+              color="grey-lighten-1"
+              icon="mdi-information"
+              variant="text"
+            ></v-btn>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </template>
 
-{{ data_ksm }}
-</template>
+<script setup>
+const files = [
+  {
+    color: 'blue',
+    icon: 'mdi-clipboard-text',
+    subtitle: 'Jan 20, 2014',
+    title: 'Vacation itinerary',
+  },
+  {
+    color: 'amber',
+    icon: 'mdi-gesture-tap-button',
+    subtitle: 'Jan 10, 2014',
+    title: 'Kitchen remodel',
+  },
+]
+const folders = [
+  {
+    subtitle: 'Jan 9, 2014',
+    title: 'Photos',
+  },
+  {
+    subtitle: 'Jan 17, 2014',
+    title: 'Recipes',
+  },
+  {
+    subtitle: 'Jan 28, 2014',
+    title: 'Work',
+  },
+]
+</script>
